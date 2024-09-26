195de7cebbdd5b7dfdbc989a879cadd8f67dfb32
Jan Schwarz <jan-paul@schw-a-rz.de>
2024-09-26 20:27:49 +0200

    insights configured

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,6 +18,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${atkinsonHyperlegible.variable} antialiased`}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
